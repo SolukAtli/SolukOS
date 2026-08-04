3f99b53fff8865299730dd7cc681cee915d225d4
SolukAtli <etanyancar@gmail.com>
2026-08-04 23:59:15 +0300

    branding: os-release, wallpaper fix, plymouth tema, hicolor ikon

--- iso/airootfs/root/customize_airootfs.sh
+++ iso/airootfs/root/customize_airootfs.sh
@@ -12,9 +12,18 @@ set -e -u
 # taniyoruz - ayni test kullanicisiyla (soluk/soluk) tutarli.
 echo "root:soluk" | chpasswd
 
-# Plymouth splash temasi (varsayilan "spinner" - sade, marka logosu
-# eklenince degistirilebilir: plymouth-set-default-theme -l ile liste)
-plymouth-set-default-theme spinner
+# Plymouth splash temasi: kendi at logolu "solukos" temamiz zaten
+# /usr/share/plymouth/themes/solukos altinda hazir (logo/word/line/glow
+# katmanlari dahil) - ama sistem varsayilan "spinner" temasini
+# kullaniyordu. Duzeltildi.
+plymouth-set-default-theme solukos
+
+# Yeni eklenen /usr/share/icons/hicolor/*/apps/solukos.png dosyalarinin
+# uygulamalar (Welcome Center, KInfoCenter vb.) tarafindan bulunabilmesi
+# icin icon cache'i yeniden olustur. Bulunamazsa build'i patlatmasin diye
+# guvenli calistiriyoruz.
+command -v gtk-update-icon-cache >/dev/null 2>&1 && \
+    gtk-update-icon-cache -f -t /usr/share/icons/hicolor || true
 
 # Saat dilimi + NTP: canli sistemde saat kaymasi Chromium/Google
 # tarafinda "bot trafigi" gibi algilanip surekli captcha'ya yol
