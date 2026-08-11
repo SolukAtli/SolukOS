7abdb449a8a14aa77b8b240abc0019a9710989c2
SolukAtli <etanyancar@gmail.com>
2026-08-12 02:10:17 +0300

    hyprland: kullanicilarin kendi temalarini yapabilecegi sistem + soluk theme komutu

--- iso-hyprland/airootfs/root/customize_airootfs.sh
+++ iso-hyprland/airootfs/root/customize_airootfs.sh
@@ -7,3 +7,8 @@ ln -sf /usr/share/zoneinfo/Europe/Istanbul /etc/localtime
 systemctl enable systemd-timesyncd.service
 
 ln -sf /dev/null /etc/systemd/system/systemd-firstboot.service
+
+# Tema sistemi: aktif temayi gosteren symlink. Ayni sebepten (Windows'ta
+# git symlink'leri koruyamayabiliyor) bunu repo'da committed bir dosya
+# olarak degil, burada, Linux build konteynerinde olusturuyoruz.
+ln -sfn themes/solukos /etc/skel/.config/soluk/current-theme
